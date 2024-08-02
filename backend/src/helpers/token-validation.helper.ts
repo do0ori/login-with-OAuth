ec440100746b087a6a41b37adce737c5a8cfe56b
do0ori <fuzzydo0ori@gmail.com>
2024-08-03 01:28:29 +0900

    feat: Add validation for refresh token
    - Inject UserTokenService into TokenValidationHelper.
    - Update validatePayload method to check iat and exp values for refresh tokens.

--- backend/src/helpers/token-validation.helper.ts
+++ backend/src/helpers/token-validation.helper.ts
@@ -2,16 +2,20 @@ import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
 
 import { AccessTokenPayload } from '../token/interfaces/access-token-payload.interface';
 import { RefreshTokenPayload } from '../token/interfaces/refresh-token-payload.interface';
+import { UserTokenService } from '../userToken/userToken.service';
 import { UsersService } from '../users/users.service';
 
 @Injectable()
 export class TokenValidationHelper {
-    constructor(private readonly usersService: UsersService) {}
+    constructor(
+        private readonly usersService: UsersService,
+        private readonly userTokenService: UserTokenService,
+    ) {}
 
     private readonly logger = new Logger(TokenValidationHelper.name);
 
     // TODO: Add return type User after implement User entity
-    async validatePayload(payload: AccessTokenPayload | RefreshTokenPayload) {
+    async validatePayload(payload: AccessTokenPayload | RefreshTokenPayload): Promise<any | never> {
         this.logger.log(payload);
 
         if (!Number.isInteger(payload.id) || payload.id < 0) {
@@ -23,6 +27,15 @@ export class TokenValidationHelper {
         if (!user) {
             this.logger.warn(`No user found with ID: ${payload.id}`);
             throw new UnauthorizedException('Invalid token.');
+        }
+
+        if (payload.type === 'refresh') {
+            const { iat, exp } = await this.userTokenService.find(user.id);
+            if (payload.iat !== iat || payload.exp !== exp) {
+                this.logger.warn(`Token iat or exp does not match for user ID: ${user.id}`);
+                // TODO: Need reathentication -> use cutom error and clear cookie using custom interceptor
+                throw new UnauthorizedException('Invalid token.');
+            }
         }
 
         return user;
