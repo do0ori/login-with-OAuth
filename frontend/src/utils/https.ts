b904ed0448082a932120032533f134091c7942a9
do0ori <fuzzydo0ori@gmail.com>
2024-08-08 01:41:55 +0900

    feat: Implement token refresh logic in axios interceptors
    - Update `createClient` to handle 401 errors and refresh tokens automatically.
    - Add `refreshAxiosInstance` for making token refresh requests.
    - Retry original request after successful token refresh to ensure seamless user experience.
    - Ensure proper handling and redirection upon token refresh failure.
    - Confirm functionality works correctly when not using `React.StrictMode`.

--- frontend/src/utils/https.ts
+++ frontend/src/utils/https.ts
@@ -2,22 +2,37 @@ import axios, { AxiosRequestConfig } from "axios";
 import { API_BASE_URL } from "../settings";
 
 export const createClient = (config?: AxiosRequestConfig) => {
-    const axiosInstance = axios.create({
+    const defaultConfig: AxiosRequestConfig = {
         baseURL: API_BASE_URL,
         timeout: 30000,
         headers: {
             "Content-Type": "application/json",
         },
         withCredentials: true,
-        ...config
-    });
+    };
+
+    const axiosInstance = axios.create({ ...defaultConfig, ...config });
+    const refreshAxiosInstance = axios.create({ ...defaultConfig });
 
     axiosInstance.interceptors.response.use(
         (response) => response,
-        (error) => {
-            if (error.response.status === 401) {
-                window.location.href = "/";
-                return;
+        async (error) => {
+            const originalRequest = error.config;
+
+            if (error.response.status === 401 && !originalRequest._retry) {
+                originalRequest._retry = true; // Mark the request as retried to avoid infinite loops.
+
+                try {
+                    // Request token refresh -> new access and refresh token will be set on cookie.
+                    await refreshAxiosInstance.post("auth/refresh");
+
+                    return axiosInstance(originalRequest); // Retry the original request with the new access token.
+                } catch (refreshError) {
+                    console.error("Token refresh failed:", refreshError);
+                    window.location.href = "/"; // Redirect to the login page.
+
+                    return Promise.reject(refreshError);
+                }
             }
 
             return Promise.reject(error);
